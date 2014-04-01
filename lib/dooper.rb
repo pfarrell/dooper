3ca957795d787511998cd7c7a7568733f8d6f3f2
Pat Farrell <pfarrell@realgravity.com>
2014-04-01 02:29:16 +0000

    first yarn routes

--- lib/dooper.rb
+++ lib/dooper.rb
@@ -2,6 +2,7 @@ require "dooper/version"
 require "dooper/s3"
 require "dooper/hdfs"
 require "dooper/hive"
+require "dooper/yarn"
 
 module Dooper
   # Your code goes here...
---
+++ lib/dooper/yarn.rb
@@ -0,0 +1,37 @@
+require 'httparty'
+
+class Yarn
+  include HTTParty
+  base_uri "master01.fire.hdp.rgops.com:8088/ws/v1"
+
+  def initialize
+  end
+
+  def build_path(resource, opts)
+    params = opts.map {|k,v| "#{k}=#{v}"}.join("&")
+    return "/#{resource}?#{params}"
+  end
+
+  def resp(resource, key, opts)
+    path = build_path(resource, opts)
+    resp = self.class.get(path)
+    return resp[key] if resp.has_key? key
+  end
+
+  def cluster_info(opts={})
+    return resp("cluster", "clusterInfo", opts)
+  end
+
+  def cluster_metrics(opts={})
+    return resp("cluster/metrics", "clusterMetrics", opts)
+  end
+
+  def applications(opts={})
+    rsp = resp("cluster/apps", "apps", opts)
+    return rsp["app"] if rsp.has_key? "app"
+  end
+
+  def application(application_id, opts={})
+    return resp("cluster/apps/#{application_id}", "app", opts)
+  end
+end
