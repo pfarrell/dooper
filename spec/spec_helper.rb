90239e986dd604dfd0ed26cfbc67a427e28cb810
Pat Farrell <pfarrell@ulive.com>
2014-04-10 08:14:06 -0700

    replaced WebMock with WebMock::API for include

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,5 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
-  config.include WebMock
+  config.include WebMock::API
 end
